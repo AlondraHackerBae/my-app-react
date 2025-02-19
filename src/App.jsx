8f7d0f4f41f16e3527e3991b29bdb83ab2ddae0f
AlondraHackerBae <khrissy921@gmail.com>
2025-02-19 12:20:18 -0600

    added weather functions and css to react app

--- src/App.jsx
+++ src/App.jsx
@@ -18,20 +18,70 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and welcome to the{" "}
-          <strong>
-            <a href="https://www.anotherdirtyglass.com" target="_blank">
-              world
+      <body>
+        <div class="container">
+          <main>
+            <header>
+              <form class="search-form" id="search-form">
+                <input
+                  type="search"
+                  placeholder="Enter a city..."
+                  required
+                  class="search-bar"
+                  id="search-bar"
+                />
+                <input type="submit" value="Search" class="search-bar-button" />
+              </form>
+            </header>
+            <div class="weather-app-data">
+              <div>
+                <h1 class="searched-city" id="city"></h1>
+                <p class="weather-details">
+                  <span id="time"></span>, <span id="description"></span>
+                  <br />
+                  Humidity: <strong id="humidity"></strong>, Wind:
+                  <strong id="wind-speed">7.2km/h</strong>
+                </p>
+              </div>
+              <div class="temperature-details">
+                <div class="icon" id="icon"></div>
+                <div class="temperature" id="temperature"></div>
+                <div class="unit">°C</div>
+              </div>
+            </div>
+            <div class="weather-forecast" id="forecast">
+              <div class="weather-forecast-day">
+                <div class="weather-forecast-date">Tue</div>
+                <div class="weather-forecast-icon">🌤</div>
+                <div class="weather-forecast-temperatures">
+                  <div class="weather-forecast-temperature">
+                    <strong>15°</strong>
+                  </div>
+                  <div class="weather-forecast-temperature">10°</div>
+                </div>
+              </div>
+            </div>
+          </main>
+          <footer>
+            This site was coded by
+            <a href="https://github.com/AlondraHackerBae" target="_blank">{" "}
+              Alondra Turner
             </a>
-          </strong>
-        </p>
-        <Weather city="Houston"/>
-      </div>
+            , is open-sourced on
+            <a
+              href="https://github.com/AlondraHackerBae/my-app-react"
+              target="_blank"
+            >{" "}
+              Github
+            </a>
+            , and hosted on
+            <a href="https://notsocloudyforecast.netlify.app/" target="_blank">
+              {" "}Netlify
+            </a>
+          </footer>
+        </div>
+        <script src="src/index.js"></script>
+      </body>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
--- src/Weather.jsx
+++ src/Weather.jsx
@@ -13,3 +13,107 @@ export default function Weather(props) {
   axios.get(apiUrl).then(handleResponse);
   return <h2>Hello Waiting on Weather</h2>;
 }
+
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  timeElement.innerHTML = formatDate(date);
+  windElement.innerHTML = `${response.data.wind.speed}mph`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  descriptionElement.innerHTML = response.data.condition.description;
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+  getForecast(response.data.city);
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+
+function searchCity(city) {
+  let apiKey = "8590002d4163ob0d0at3864bcd25fb7b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-bar");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "8590002d4163ob0d0at3864bcd25fb7b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+  console.log(apiUrl);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <div class="weather-forecast-icon"> <img src="${
+          day.condition.icon_url
+        }" class="weather-forecast-icon" /></div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>${Math.round(day.temperature.maximum)}°</strong>
+          </div>
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}°</div>
+        </div>
+      </div>
+    `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+let searchBarElement = document.querySelector("#search-form");
+searchBarElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Houston");
+getForecast("Houston");
